34b71fa98cfb6a015f0c66c14719403df838e350
DavidTZC <80299276+DavidTZC@users.noreply.github.com>
2021-03-09 10:55:29 +0800

    Update mail.py

--- main/mail.py
+++ main/mail.py
@@ -8,22 +8,22 @@ from email.utils import formataddr
 
 def wechat(text, desp):
     return requests.get(
-        "https://sc.ftqq.com/SCU79675Tbfd23351bd3ed5501aae715beddfbdbf5e3a123f8fb98.send?text=" + text + "&desp=" + desp).json()
+        "https://sc.ftqq.com/SCU164225T52e7796539ad296f29e329de86a417f46046e3310caaf.send?text=" + text + "&desp=" + desp).json()
 
 
 def send(mail, username):
     # 发送到的邮箱地址，收件人的名称
     try:
-        sender = '3104182180@qq.com'  # 发件人邮箱账号
+        sender = 'tzc_david@163.com'  # 发件人邮箱账号
         msg = MIMEText("To：" + username + "\n\t自动健康打卡成功通知\n\t通知发送时间：" + time.strftime(
             "%Y/%m/%d %H:%M") + "\n\n\t\tFrom：运行在github的开源项目", 'plain', 'utf-8')  # 填写邮件内容
         # 括号里的对应发件人邮箱昵称、发件人邮箱账号
         msg['From'] = formataddr(["健康打卡通知", sender])
         msg['To'] = formataddr([username, mail])  # 括号里的对应收件人邮箱昵称、收件人邮箱账号
         msg['Subject'] = "健康打卡成功通知"  # 邮件的主题，也可以说是标题
-        server = smtplib.SMTP_SSL("smtp.qq.com", 465)  # 发件人邮箱中的SMTP服务器，端口是25
+        server = smtplib.SMTP_SSL("smtp.163.com", 465)  # 发件人邮箱中的SMTP服务器，端口是25
         # 括号中对应的是发件人邮箱账号、邮箱的SMTP中生成的授权码
-        server.login(sender, 'toqqwfcxsumgdgbf')
+        server.login(sender, 'CBVRANJUYSJLYQVT')
         # 括号中对应的是发件人邮箱账号、收件人邮箱账号、发送邮件
         server.sendmail(sender, mail, msg.as_string())
         server.quit()  # 关闭连接
