f0576fa4506414027de3504ed90a9233e2b1bcf4
DavidTZC <80299276+DavidTZC@users.noreply.github.com>
2021-05-21 12:23:53 +0800

    Update main.py

--- main/main.py
+++ main/main.py
@@ -1,14 +1,21 @@
 # coding=utf-8
 import requests
-import getinfo
-import mail
 
-def run(user, UA, cook):
+import getinfo
+
+
+def run(studata, UA, cook):
+    """获取处理后的数据
+    :param studatae:学生信息
+    :param UA:传入的UA
+    :param cook:传入的cookie
+    :return :打卡结果
+    """
     # 读取个人提交信息
-    info = getinfo.data(UA, cook)
+    info = getinfo.data(studata, UA, cook)
     if info == 0:
         print("今日打卡已完成，自动打卡取消\n")
-        return
+        return "已完成"
     # 提交今日打卡
     url = 'https://yq.weishao.com.cn/api/questionnaire/questionnaire/addMyAnswer'
     head = {
@@ -30,12 +37,13 @@ def run(user, UA, cook):
     data = requests.post(url, json=info, headers=head).json()
     if data.get("errcode") == 0:
         print("打卡成功！")
-        if user.get("notice") == "true":
-            print("正在发送邮件···")
-            mail.send(user.get("email"), user.get("name"))
+        return "成功！"
 
-    elif data.get("errcode") == 500:
+    # 下面这条if基本上没有运行不到，在上面已经出现了这种情况了
+    elif data.get("status") == 400:
         print("今日打卡已完成，自动打卡取消\n")
+        return "已完成"
+
     else:
-        print("未知的errcode\n" + str(data))
-        mail.wechat("自动打卡遇到未知的返回值", str(data))
+        print("---未知的errcode\n" + str(data) + "\n")
+        return "未知结果"
